b0d995466759e87580898a33b8613f04fd0634e6
bekatadesse <bekatadesse5@gmail.com>
2022-11-16 21:40:34 +0300

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
     result = local("sudo tar -cvzf {} web_static".format(filename))
-    
+
     if result.failed:
         return None
     return filename
