e854813daf364bbc8d2d09cd414882b3ce9c29d0
bekatadesse <bekatadesse5@gmail.com>
2022-11-21 17:33:55 +0300

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+""" Starts a Flash Web Application HBNB"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
